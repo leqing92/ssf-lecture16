42c86cc72f958e53302a994adc922aa4470cef20
leqing92 <leqing92@hotmail.com>
2024-03-14 10:44:24 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/day16lecture/Day16LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day16lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day16LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day16LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day16lecture/Day16LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day16lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day16LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
